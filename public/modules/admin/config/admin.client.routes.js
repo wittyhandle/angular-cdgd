682aba8b98fb3a3a2608af821f2d5ec5c5f0c8dc
wittyhandle <mikeottinger@gmail.com>
2014-08-19 22:23:08 -0700

    added the beginnings of the login form

---
+++ public/modules/admin/config/admin.client.routes.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Setting up route
+angular.module('admin').config(['$stateProvider',
+    function($stateProvider) {
+
+        $stateProvider.
+            state('login', {
+                url: '/login',
+                templateUrl: 'modules/admin/views/login.client.view.html'
+            });
+    }
+]);
--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -3,7 +3,8 @@
 // Setting up route
 angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 	function($stateProvider, $urlRouterProvider) {
-		// Redirect to home view when route not found
+
+        // Redirect to home view when route not found
 		$urlRouterProvider.otherwise('/');
 
 		// Home state routing
