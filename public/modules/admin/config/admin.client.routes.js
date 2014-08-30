a5e091591da06fc07da37a80fc4fb0257a6e5bfb
wittyhandle <mikeottinger@gmail.com>
2014-08-30 14:37:10 -0700

    re-structure the http interceptor function

--- public/modules/admin/config/admin.client.routes.js
+++ public/modules/admin/config/admin.client.routes.js
@@ -33,14 +33,32 @@ angular.module('admin').config(['$stateProvider',
             }
         ]);
     }
-]).factory('AuthInterceptor', ['$location',
-    function($location)
+]).factory('AuthInterceptor', ['$location', '$q',
+    function($location, $q)
     {
         return {
+            request: function (config) {
+                return config || $q.when(config);
+            },
+            requestError: function(request){
+                return $q.reject(request);
+            },
+            response: function (response) {
+                return response || $q.when(response);
+            },
+            responseError: function (response)
+            {
+                if (response && response.status === 401)
+                {
+                    $location.path('/login');
+                }
+                else
+                {
+                    return $q.reject(response);
+                }
 
-            responseError: function(response) {
-                $location.path('/login');
             }
         };
+
     }
 ]);
